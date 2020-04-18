ffd6a86af2f8d47678a8f906cc1826d614e57312
dansteryoo <dansteryoo@gmail.com>
2020-04-18 12:48:00 -0400

    heroku init commmit

--- app/views/api/devos/show.json.jbuilder
+++
@@ -1,6 +0,0 @@
-json.partial! '/api/devos/devo', devo: @devo
-    json.notes @devo.notes do |note| 
-      json.note note 
-      json.notary note.notary
-    end
-    json.images @devo.images.map { |image| url_for(image) }
--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import Modal from './splash/modal';
 import SplashContainer from './splash/splash_container';
+import SignUpContainer from './session_form/signup_form_container';
 import HomePageContainer from './home/home_container';
 
 const App = () => (
@@ -15,6 +16,7 @@ const App = () => (
 
         <Switch>
             <AuthRoute exact path='/' component={SplashContainer} />
+            <AuthRoute exact path='/wrt/sign_up' component={SignUpContainer} />
         </Switch>
 
     </div>
