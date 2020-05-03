2e9dc62f241523d8938a0a75eb708955c36a306f
Danny Yoo <dansteryoo@gmail.com>
2020-05-03 13:22:32 -0400

    uncomment seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,104 +7,104 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-# Devo.destroy_all
+Devo.destroy_all
 
-# he_data = JSON.parse(File.read("#{Rails.root}/app/assets/seed/hereadstruth.json"))
+he_data = JSON.parse(File.read("#{Rails.root}/app/assets/seed/hereadstruth.json"))
 
-#   he_data.each do |each|
+  he_data.each do |each|
 
-#     if each["img_url"] == ""
-#         @img = "null"
-#     else 
-#         @img = each["img_url"]
-#     end
+    if each["img_url"] == ""
+        @img = "null"
+    else 
+        @img = each["img_url"]
+    end
 
-#     if each["devo_passages"] == ""
-#         @passages = "null"
-#     else 
-#         @passages = each["devo_passages"]
-#     end
+    if each["devo_passages"] == ""
+        @passages = "null"
+    else 
+        @passages = each["devo_passages"]
+    end
 
-#     if each["devo_summary"] == ""
-#         @summary = "null"
-#     else 
-#         @summary = each["devo_summary"]
-#     end
+    if each["devo_summary"] == ""
+        @summary = "null"
+    else 
+        @summary = each["devo_summary"]
+    end
 
-#     if each["devo_title"] == ""
-#         @title = "null"
-#     else 
-#         @title = each["devo_title"]
-#     end
+    if each["devo_title"] == ""
+        @title = "null"
+    else 
+        @title = each["devo_title"]
+    end
 
-#     if each["book_title"].split(' ').join('') == ""
-#         @book = "null"
-#     else 
-#         @book = each["book_title"].split(' ').join('')
-#     end
+    if each["book_title"].split(' ').join('') == ""
+        @book = "null"
+    else 
+        @book = each["book_title"].split(' ').join('')
+    end
 
-#     # puts ""
-#     # puts @summary
-#     # puts ""
+    # puts ""
+    # puts @summary
+    # puts ""
 
-#     # byebug
-#     Devo.create!(
-#     gender: "HE",
-#     book: @book,
-#     title: @title,
-#     passages: @passages,
-#     summary: @summary,
-#     img: @img,
-#         )
-#     # byebug
-#   end
+    # byebug
+    Devo.create!(
+    gender: "HE",
+    book: @book,
+    title: @title,
+    passages: @passages,
+    summary: @summary,
+    img: @img,
+        )
+    # byebug
+  end
 
-#   she_data = JSON.parse(File.read("#{Rails.root}/app/assets/seed/shereadstruth.json"))
+  she_data = JSON.parse(File.read("#{Rails.root}/app/assets/seed/shereadstruth.json"))
 
-#   she_data.each do |each|
+  she_data.each do |each|
 
-#     if each["img_url"] == ""
-#         @img = "null"
-#     else 
-#         @img = each["img_url"]
-#     end
+    if each["img_url"] == ""
+        @img = "null"
+    else 
+        @img = each["img_url"]
+    end
 
-#     if each["devo_passages"] == ""
-#         @passages = "null"
-#     else 
-#         @passages = each["devo_passages"]
-#     end
+    if each["devo_passages"] == ""
+        @passages = "null"
+    else 
+        @passages = each["devo_passages"]
+    end
 
-#     if each["devo_summary"] == ""
-#         @summary = "null"
-#     else 
-#         @summary = each["devo_summary"]
-#     end
+    if each["devo_summary"] == ""
+        @summary = "null"
+    else 
+        @summary = each["devo_summary"]
+    end
 
-#     if each["devo_title"] == ""
-#         @title = "null"
-#     else 
-#         @title = each["devo_title"]
-#     end
+    if each["devo_title"] == ""
+        @title = "null"
+    else 
+        @title = each["devo_title"]
+    end
 
-#     if each["book_title"].split(' ').join('') == ""
-#         @book = "null"
-#     else 
-#         @book = each["book_title"].split(' ').join('')
-#     end
+    if each["book_title"].split(' ').join('') == ""
+        @book = "null"
+    else 
+        @book = each["book_title"].split(' ').join('')
+    end
 
-#     # puts ""
-#     # puts @summary
-#     # puts ""
+    # puts ""
+    # puts @summary
+    # puts ""
 
-#     # byebug
-#     Devo.create!(
-#     gender: "SHE",
-#     book: @book,
-#     title: @title,
-#     passages: @passages,
-#     summary: @summary,
-#     img: @img,
-#         )
-#     # byebug
-#   end
+    # byebug
+    Devo.create!(
+    gender: "SHE",
+    book: @book,
+    title: @title,
+    passages: @passages,
+    summary: @summary,
+    img: @img,
+        )
+    # byebug
+  end
