83808d5c51dc592520c95ae8e8cd95bb3f33cd03
Danny Yoo <dansteryoo@gmail.com>
2020-05-15 17:30:32 -0400

    notes form confirmation success

--- frontend/components/notes/notes_form.jsx
+++ frontend/components/notes/notes_form.jsx
@@ -8,32 +8,48 @@ class NotesForm extends React.Component {
             title: '',
             category: '',
             tags: '',
-            body: ''
+            body: '',
+            success: false,
         }
 
-
         this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    };
 
     componentDidMount() {
 
-    }
+    };
 
     componentWillUnmount() {
         this.props.clearErrors();
-    }
+    };
 
     update(f) {
         return e => this.setState({
             [f]: e.target.value
-        });
-    }
+        })
+    };
 
     handleSubmit(e) {
         e.preventDefault();
         let note = Object.assign({}, this.state);
-        this.props.createNote(note);
-    }
+        this.props.createNote(note)
+            .then(() => {
+                this.setState({ 
+                    success: true,
+                    title: '',
+                    category: '',
+                    tags: '',
+                    body: '',
+                })
+            })
+            .then(() => this.renderSuccessMsg())
+    };
+
+    renderSuccessMsg() {
+        window.setTimeout(() => {
+            this.setState({ success: false })
+        }, 4000)
+    };
 
     renderErrors() {
         return (
@@ -42,123 +58,78 @@ class NotesForm extends React.Component {
                     <li key={`error-${i}`}>{error}</li>
                 ))}
             </ul>
-        );
-    }
+        )
+    };
 
     render() {
-
-    // debugger
-        return (
-            <>
-                <div className='notes-form-container'>
-                    <form onSubmit={this.handleSubmit} >
-                        <div className='notes-form'>
-
-                            {/* title */}
-                            <label>Title</label>
-                            <input type='text'
-                                className='notes-form-input-title'
-                                value={this.state.title}
-                                // placeholder={'Title'}
-                                onChange={this.update('title')}
-                            // required
-                            />
-
-                            {/* body */}
-                            <label>Body</label>
-                            <textarea 
-                                className='notes-form-textarea'
-                                value={this.state.body}
-                                placeholder={'Enter note here..'}
-                                onChange={this.update('body')}
-                            // required
-                            />
-
-                            {/* categories and tags */}
-
-                            <div className='notes-form-bottom'> 
-                                <label>Category</label>
-                                <input type='text'
-                                    className='notes-form-input'
-                                    value={this.state.category}
-                                    // placeholder={'category'}
-                                    onChange={this.update('category')}
-                                // required   
-                                />
-                                <label>#Tags</label>
-                                <input type='text'
-                                    className='notes-form-input'
-                                    value={this.state.tags}
-                                    // placeholder={'#tags'}
-                                    onChange={this.update('tags')}
-                                // required   
-                                />
-                            </div>
-                            {this.renderErrors()}
-                            <div className='button-container'>
-                                <button className='notes-form-submit-button' type='submit'>
-                                    Submit
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-
-                    <br />
+        if (this.state.success) {
+            return (
+                <div className='success-message-div'>
+                    <span>Note Created!</span>
                 </div>
-            </>
-        );
+            )
+        } else {
+            return (
+                <>
+                    <div className='notes-form-container'>
+                        <form onSubmit={this.handleSubmit} >
+                            <div className='notes-form'>
+
+                                {/* title */}
+                                <label>Title</label>
+                                <input type='text'
+                                    className='notes-form-input-title'
+                                    value={this.state.title}
+                                    // placeholder={'Title'}
+                                    onChange={this.update('title')}
+                                // required
+                                />
+
+                                {/* body */}
+                                <label>Body</label>
+                                <textarea 
+                                    className='notes-form-textarea'
+                                    value={this.state.body}
+                                    placeholder={'Enter note here..'}
+                                    onChange={this.update('body')}
+                                // required
+                                />
+
+                                {/* categories and tags */}
+
+                                <div className='notes-form-bottom'> 
+                                    <label>Category</label>
+                                    <input type='text'
+                                        className='notes-form-input'
+                                        value={this.state.category}
+                                        // placeholder={'category'}
+                                        onChange={this.update('category')}
+                                    // required   
+                                    />
+                                    <label>#Tags</label>
+                                    <input type='text'
+                                        className='notes-form-input'
+                                        value={this.state.tags}
+                                        // placeholder={'#tags'}
+                                        onChange={this.update('tags')}
+                                    // required   
+                                    />
+                                </div>
+                                {this.renderErrors()}
+                                <div className='button-container'>
+                                    <button className='notes-form-submit-button' type='submit'>
+                                        Submit
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+
+                        <br />
+                    </div>
+                </>
+            );
+        }
     }
 }
 
 export default NotesForm;
-
-
-// constructor(props) {
-//     super(props);
-//     this.state = {
-//         fullName: null,
-//         email: null,
-//         password: null,
-//         errors: {
-//             fullName: '',
-//             email: '',
-//             password: '',
-//         }
-//     };
-// }
-
-// handleChange = (event) => {
-//     event.preventDefault();
-//     const { name, value } = event.target;
-//     let errors = this.state.errors;
-
-//     switch (name) {
-//         case 'fullName':
-//             errors.fullName =
-//                 value.length < 5
-//                     ? 'Full Name must be 5 characters long!'
-//                     : '';
-//             break;
-//         case 'email':
-//             errors.email =
-//                 validEmailRegex.test(value)
-//                     ? ''
-//                     : 'Email is not valid!';
-//             break;
-//         case 'password':
-//             errors.password =
-//                 value.length < 8
-//                     ? 'Password must be 8 characters long!'
-//                     : '';
-//             break;
-//         default:
-//             break;
-//     }
-
-//     this.setState({ errors, [name]: value }, () => {
-//         console.log(errors)
-//     })
-// }
-
-// const validEmailRegex =
-//     RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
