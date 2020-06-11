4e363f4c6853878a3f9d225b46a830a5a06f395a
Danny Yoo <dansteryoo@gmail.com>
2020-06-11 13:40:47 -0400

    refactor created bookTitles.js

--- frontend/components/modal_pages/category_list_NT.jsx
+++ frontend/components/modal_pages/category_list_NT.jsx
@@ -1,48 +1,17 @@
 import React from 'react';
+import { NTbooks, NTbookFormat } from '../home/sidenav/bookTitles'
 
 const CategoryListNT = ({ eachDevoTitle, handleClick }) => {
 
-    const NTbooks = [
-        "Matthew",
-        "Mark",
-        "Luke",
-        "John",
-        "ActsoftheApostles",
-        "Romans",
-        "1&2Corinthians",
-        "Galatians",
-        "Ephesians",
-        "Philippians",
-        "Colossians",
-        "1&2Thessalonians",
-        "1&2TimothyandTitus",
-        "Philemon",
-        "Hebrews",
-        "James",
-        "1&2Peter",
-        "123John",
-        "Jude",
-        "Revelation",
-    ];
-
-    const NTbookFormat = {
-        "1&2thessalonians": "1 & 2 Thessalonians",
-        "1&2peter": "1 & 2 Peter",
-        "1&2timothyandtitus": "1 & 2 Timothy, & Titus",
-        "actsoftheapostles": "Acts",
-        "1&2corinthians": "1 & 2 Corinthians"
-    };
-
-    const lowerCaseTitle = NTbooks.map(ele => ele.toLowerCase());
+    const lowerCaseArr = NTbooks.map(ele => ele.toLowerCase());
+    const inBookTitle = lowerCaseArr.includes(eachDevoTitle.book)
+    const isbookTitleDefined = NTbookFormat[eachDevoTitle.book] !== undefined
+    const bookTitle = NTbooks[lowerCaseArr.indexOf(eachDevoTitle.book)]
 
     let NTbook;
-    const inBookTitle = lowerCaseTitle.includes(eachDevoTitle.book)
-    const bookTitleUndefined = NTbookFormat[eachDevoTitle.book] === undefined
-    const bookTitle = NTbooks[lowerCaseTitle.indexOf(eachDevoTitle.book)]
-
-    if (inBookTitle && bookTitleUndefined) {
+    if (inBookTitle && !isbookTitleDefined) {
         NTbook = bookTitle
-    } else if (inBookTitle && !bookTitleUndefined) {
+    } else if (inBookTitle && isbookTitleDefined) {
         NTbook = NTbookFormat[eachDevoTitle.book]
     };
 
