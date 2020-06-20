23e489b2b709180a062379cd2959a007c7afa465
Danny Yoo <dansteryoo@gmail.com>
2020-06-20 17:50:00 -0400

    mobile responsive ready

--- frontend/components/nav/navbar.jsx
+++ frontend/components/nav/navbar.jsx
@@ -72,7 +72,9 @@ class NavBar extends React.Component {
             Notes
           </li>
           <li className="profile-li">
-            <a onClick={() => this.props.openModal('Profiles')}>Profile</a>
+            <a onClick={() => this.props.openModal('Profiles')}>
+            Profile
+            </a>
             <ul className="dropdown-profile">
               <li className='logout-li' onClick={() => this.props.logout()}>
                 Logout
