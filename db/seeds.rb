ac42e1a2b8c3cdafb0013d5e8b69c00a5a059cdd
Danny Yoo <dansteryoo@gmail.com>
2020-06-20 23:59:17 -0400

    he data cleaned

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Devo.destroy_all
+Devo.where(gender: "HE").destroy_all
 
 he_data_1 = JSON.parse(File.read("#{Rails.root}/app/assets/seed/hereadstruth_v1.json"))
 he_data_2 = JSON.parse(File.read("#{Rails.root}/app/assets/seed/hereadstruth_v2.json"))
@@ -63,57 +63,57 @@ heDataArray.each do |he_data|
     # byebug
 end
 
-she_data_1 = JSON.parse(File.read("#{Rails.root}/app/assets/seed/shereadstruth_v1.json"))
-she_data_2 = JSON.parse(File.read("#{Rails.root}/app/assets/seed/shereadstruth_v2.json"))
+# she_data_1 = JSON.parse(File.read("#{Rails.root}/app/assets/seed/shereadstruth_v1.json"))
+# she_data_2 = JSON.parse(File.read("#{Rails.root}/app/assets/seed/shereadstruth_v2.json"))
 
-sheDataArray = [she_data_1, she_data_2]
+# sheDataArray = [she_data_1, she_data_2]
 
-sheDataArray.each do |she_data|
-    she_data.each do |each|
+# sheDataArray.each do |she_data|
+#     she_data.each do |each|
 
-        if each["img_url"] == ""
-            @img = "null"
-        else 
-            @img = each["img_url"]
-        end
+#         if each["img_url"] == ""
+#             @img = "null"
+#         else 
+#             @img = each["img_url"]
+#         end
 
-        if each["devo_passages"] == ""
-            @passages = "null"
-        else 
-            @passages = each["devo_passages"]
-        end
+#         if each["devo_passages"] == ""
+#             @passages = "null"
+#         else 
+#             @passages = each["devo_passages"]
+#         end
 
-        if each["devo_summary"] == ""
-            @summary = "null"
-        else 
-            @summary = each["devo_summary"]
-        end
+#         if each["devo_summary"] == ""
+#             @summary = "null"
+#         else 
+#             @summary = each["devo_summary"]
+#         end
 
-        if each["devo_title"] == ""
-            @title = "null"
-        else 
-            @title = each["devo_title"]
-        end
+#         if each["devo_title"] == ""
+#             @title = "null"
+#         else 
+#             @title = each["devo_title"]
+#         end
 
-        if each["book_title"].split(' ').join('') == ""
-            @book = "null"
-        else 
-            @book = each["book_title"].split(' ').join('')
-        end
+#         if each["book_title"].split(' ').join('') == ""
+#             @book = "null"
+#         else 
+#             @book = each["book_title"].split(' ').join('')
+#         end
 
-        # puts ""
-        # puts @summary
-        # puts ""
-        # byebug
-            Devo.create!(
-                gender: "SHE",
-                book: @book,
-                title: @title,
-                passages: @passages,
-                summary: @summary,
-                img: @img,
+#         # puts ""
+#         # puts @summary
+#         # puts ""
+#         # byebug
+#             Devo.create!(
+#                 gender: "SHE",
+#                 book: @book,
+#                 title: @title,
+#                 passages: @passages,
+#                 summary: @summary,
+#                 img: @img,
                 
-            )
-    # byebug
-    end
-end
+#             )
+#     # byebug
+#     end
+# end
